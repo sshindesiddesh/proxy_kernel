c7e82c0a7cc176d08761c1700a675bbe82584693
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-10-21 04:17:15 -0400

    [Print Timer and Key] Add support to loop and not shutdown the system
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- src/proxy_kernel.c
+++ src/proxy_kernel.c
@@ -13,13 +13,18 @@ asm (".globl _start\n\
 
 void entry()
 {
-  const char* message =
-"This is CSE506 proxy_kernel. Still in beta phase.. \n";
-  while (*message)
-    sbi_console_putchar(*message++);
-  kprintf("Hello world\n");
-  int a = 10;
-  char b = 'A';
-  kprintf("%d %c", a, b);
-  sbi_shutdown();
+	const char* message = "This is CSE506 proxy_kernel. Still in beta phase.. \n";
+	while (*message)
+		sbi_console_putchar(*message++);
+	kprintf("Welcome To SBU Unix on RISC V\n");
+	memory_block_info mb;
+
+	sbi_query_memory(sbi_hart_id(), &mb);
+	kprintf("Memory Info : %p %x %x\n", mb.base, mb.size, mb.node_id);
+	while (1) {
+		char ch;
+		ch = sbi_console_getchar();
+		//sbi_console_putchar(ch);
+	}
+	sbi_shutdown();
 }
